11222ce7244fe7555720393fc4236335d5f7f8bd
תכנות <@690156GUR.LOCAL>
2023-07-19 14:49:58 +0300

    add

--- modules/teachers.js
+++ modules/teachers.js
@@ -2,52 +2,71 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const { deleteTeacherSchedule } = require('./teacher_schedule')
-const { dbServer, postData } = require('../services/axios');
+const { dbServer, getData, postData } = require('../services/axios');
 const { MONGO_TEACHERS_COLLECTION } = process.env;
-
 
 // insert  //
 async function insertTeacher(obj) {
-    const name = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERS_COLLECTION, filter: { name: obj.name } })
-    if (name.data[0])
-        return "the name is exist"
-    const ans = await postData(dbServer, '/crud_db/insert', { entity: MONGO_TEACHERS_COLLECTION, ...obj })
-    return ans.data;
+    try {
+        console.log("try");
+        console.log({ obj });
+        console.log("entity:", `tbl_${obj.entity}`, "name:", obj.values.TeacherName);
+        const name = await postData(dbServer, '/read_db/readMany', { entity: `tbl_${obj.entity}`, condition: obj.values.TeacherName })
+        console.log(name);
+        if (name.data[0]) {
+            return "the name is exist"
+        }
+        else {
+            const ans = await postData(dbServer, '/create_db/createOne', { ...obj })
+            return ans;
+        }
+    }
+    catch (error) {
+        console.log("error");
+        throw error
+    }
 }
 
 // delete //
 async function deleteTeacher(TeacherName) {
-    const ans = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERS_COLLECTION, filter: { ...TeacherName } })
+    const ans = await postData(dbServer, '/read_db/read', { table_name: MONGO_TEACHERS_COLLECTION, filter: { ...TeacherName } })
     try {
         if (ans.data[0]) {
-            await postData(dbServer, '/crud_db/delete', { entity: MONGO_TEACHERS_COLLECTION, filter: { ...TeacherName } })
+            await postData(dbServer, '/delete_db/deleteOne', { table_name: MONGO_TEACHERS_COLLECTION, filter: { ...TeacherName } })
             deleteTeacherSchedule(ans.data[0]._id.toString())
-            addDisabledTeacher({ name: ans.data[0].name, phone: ans.data[0].phone, address: ans.data[0].address,annotation:ans.data[0].annotation,email:ans.data[0].email });
+            addDisabledTeacher({ name: ans.data[0].name, phone: ans.data[0].phone, address: ans.data[0].address, annotation: ans.data[0].annotation, email: ans.data[0].email });
         }
         return true;
     }
-    catch {
-        return false;
+    catch (error) {
+        throw error;
     }
 }
 
 // read //
 async function findTeacher(obj) {
-    const ans = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERS_COLLECTION, filter: { ...obj } })
-    return ans.data;
+    try {
+        const ans = await postData(dbServer, '/read_db/read', { table_name: MONGO_TEACHERS_COLLECTION, filter: { ...obj } })
+        if (ans.data[0])
+            return ans;
+        return 'the teacher does not exist'
+    }
+    catch (error) {
+        throw error
+    }
 }
 
 //update //
 async function updateTeacher(name, update) {
-    let ans = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERS_COLLECTION, filter: { ...name } })
+    let ans = await postData(dbServer, '/read_db/read', { table_name: MONGO_TEACHERS_COLLECTION, filter: { ...name } })
     if (ans.data[0]) {
         if (name.name !== update.name) {
-            const teacher = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERS_COLLECTION, filter: { name: update.name } })
+            const teacher = await postData(dbServer, '/read_db/read', { table_name: MONGO_TEACHERS_COLLECTION, filter: { name: update.name } })
             if (teacher.data[0]) {
                 return "the name is exist";
             }
         }
-        ans = await postData(dbServer, '/crud_db/update', { entity: MONGO_TEACHERS_COLLECTION, filter: { ...name }, update: { "$set": { ...update } } })
+        ans = await postData(dbServer, '/update_db/updateOne', { table_name: MONGO_TEACHERS_COLLECTION, filter: { ...name }, update: { "$set": { ...update } } })
     }
 
     return ans.data
@@ -56,35 +75,26 @@ async function updateTeacher(name, update) {
 
 // insert To SqlDelete Teacher //
 async function addDisabledTeacher(obj) {
-    await postData(dbServer, '/crud_db/insert', { entity: 'teachers', values: `'${obj.name}','${obj.phone}','${obj.address.city}','${obj.address.street}','${obj.address.zip}','${obj.email}','${obj.annotation}'` })
+    try {
+        const ans = await postData(dbServer, '/create_db/createOne', { table_name: 'teachers', values: `'${obj.name}','${obj.phone}','${obj.address.city}'` })
+        return ans
+    }
+    catch (error) {
+        throw error
+    }
 }
 
 // read from Sql //
 async function findDisabledTeachers() {
-    const ans = await postData(dbServer, '/crud_db/read', { entity: "teachers", columns: '*', condition: '1=1' })
-    if (ans.data)
-        return ans.data
-    return "there is no data"
-
-
-
+    try {
+        const ans = await postData(dbServer, '/read_db/read', { table_name: "teachers", columns: '*', condition: '1=1' })
+        if (ans.data[0])
+            return ans
+        return "data does not exist"
+    }
+    catch (error) {
+        throw error
+    }
 }
 
-
-
-
-
-
 module.exports = { insertTeacher, deleteTeacher, findTeacher, updateTeacher, findDisabledTeachers }
-
-
-
-
-
-
-
-
-
-
-
-
--- routers/teachers.js
+++ routers/teachers.js
@@ -2,56 +2,63 @@ const express = require('express')
 const router = express.Router()
 const { deleteTeacher, insertTeacher, findTeacher, updateTeacher, findDisabledTeachers } = require('../modules/teachers')
 
-
 // insert  //
 router.post('/insertTeacher', express.json(), async (req, res) => {
-    const ans = await insertTeacher(req.body)
-    res.send(ans)
+    try {
+        const ans = await insertTeacher(req.body)
+        res.status(ans.status).send(ans.data)
+    }
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
 
 //delete//
 router.post('/deleteTeacher', express.json(), async (req, res) => {
-    const ans = await deleteTeacher(req.body);
-    res.send(ans)
+    try {
+        const ans = await deleteTeacher(req.body);
+        res.status(ans.status).send(ans.data)
+    }
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
-
 
 // read //
 router.get('/findTeacher', async (req, res) => {
-    
-    const ans = await findTeacher(req.query)
-    if (ans)
-        res.status(200).send(ans)
-    else
-        res.status(404).send('dont find teacher for your ')
-
+    try {
+        const ans = await findTeacher(req.query)
+        res.status(ans.status).send(ans.data)
+    }
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
-router.post('/findTeacherByFilter',express.json(),async(req,res)=>{
-    
-const ans = await findTeacher({$or:req.body.filter})
-res.send(ans)
+router.post('/findTeacherByFilter', express.json(), async (req, res) => {
+    const ans = await findTeacher({ $or: req.body.filter })
+    res.send(ans)
 })
-
-
-
 
 //update //
 router.post('/updateTeacher', express.json(), async (req, res) => {
-  
-    const ans = await updateTeacher({ name: req.body.name }, req.body.update)
-    if (ans)
-        res.status(200).send(ans)
-
+    try {
+        const ans = await updateTeacher({ name: req.body.name }, req.body.update)
+        res.status(ans.status).send(ans.data)
+    }
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
-
 
 //read from sql//
 router.get('/findDisabledTeachers', async (req, res) => {
-    const ans = await findDisabledTeachers()
-        res.status(200).send(ans)
-
+    try {
+        const ans = await findDisabledTeachers()
+        res.status(ans.status).send(ans.data)
+    }
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
 
 module.exports = router
-
-
