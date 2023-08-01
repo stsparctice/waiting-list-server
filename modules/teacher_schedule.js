c3c6879dee08aa0c19fe484f225485a6b502d1e8
תכנות <@690156GUR.LOCAL>
2023-08-01 18:19:54 +0300

    add function

--- modules/teacher_schedule.js
+++ modules/teacher_schedule.js
@@ -1,73 +1,90 @@
 require('dotenv').config()
-const { MONGO_TEACHERSCHEDULE_COLLECTION, MONGO_TEACHERS_COLLECTION } = process.env;
+const { MONGO_TEACHERSCHEDULE_COLLECTION } = process.env;
 const { dbServer, postData } = require('../services/axios');
-
 
 // insert
 async function insertTeacherSchedule(obj) {
-    obj.name = await findTeacherScheduleByTeacherName(obj.name)
-    if (obj.name) {
-        const schedule = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, filter: { name: obj.name } })
-
-        if (!schedule.data[0]) {
-            const ans = await postData(dbServer, '/crud_db/insert', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, ...obj })
-            return ans.data;
+    try {
+        obj.name = await findTeacherScheduleByTeacherName(obj.name)
+        if (obj.name) {
+            const schedule = await postData(dbServer, '/read/readOne/TeacherSchedule', { condition: { Disabled: 0 } })
+            if (!schedule.data[0]) {
+                const ans = await postData(dbServer, '/create/insert', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, ...obj })
+                return ans.data;
+            }
         }
-
+        return "the name is not exist"
     }
-    return "the name is not exist"
-
+    catch (error) {
+        throw error
+    }
 }
 
 //delete
 async function deleteTeacherSchedule(id) {
-    await postData(dbServer, '/crud_db/delete', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, filter: { name: id } })
+    try {
+        const ans = await postData(dbServer, '/crud_db/delete', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, filter: { name: id } })
+    }
+    catch (error) {
 
+    }
 }
 
 //update
 async function updateTeacherSchedule(name, update) {
-    name = await findTeacherScheduleByTeacherName(name)
-    if (name) {
-        ans = await postData(dbServer, '/crud_db/update', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, filter: { name: name }, update: { "$set": { ...update } } })
-        return ans.data;
+    try {
+        name = await findTeacherScheduleByTeacherName(name)
+        if (name) {
+            ans = await postData(dbServer, '/crud_db/update', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, filter: { name: name }, update: { "$set": { ...update } } })
+            return ans.data;
 
+        }
+        return "the name is not exist"
     }
-    return "the name is not exist"
+    catch (error) {
+        throw error
+    }
 }
 
 
 // read 
 async function findTeachersSchedule(obj) {
-    const ans = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, filter: { ...obj } })
-    return ans.data;
+    try {
+        const ans = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, filter: { ...obj } })
+        return ans.data;
+    }
+    catch (error) {
+        throw error
+    }
 }
 
 async function findTeacherScheduleToSpecificTeacher(name) {
-    name = await findTeacherScheduleByTeacherName(name)
-    if (name) {
-        ans = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, filter: { name: name } })
-        return ans.data;
+    try {
+        name = await findTeacherScheduleByTeacherName(name)
+        if (name) {
+            ans = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERSCHEDULE_COLLECTION, filter: { name: name } })
+            return ans.data;
+        }
+        return "the name is not exist"
     }
-    return "the name is not exist"
-
-
+    catch (error) {
+        throw error
+    }
 }
 
 async function findTeacherScheduleByTeacherName(name) {
-    const id = await postData(dbServer, '/crud_db/read', { entity: MONGO_TEACHERS_COLLECTION, filter: { name: name }, project: { _id: 1 } })
-    if (id.data[0]) {
-        return id.data[0]._id.toString();
+    try {
+        const ans = await postData(dbServer, '/read/readMany/Teachers', { condition: { TecherName: `'${name}'`, Disabled: 0 } })
+        if (id.data[0]) {
+            return id.data[0]._id.toString();
+        }
+        else {
+            return false;
+        }
     }
-    else {
-        return false;
+    catch (error) {
+        throw error
     }
-
-    
 }
 
-
-
-
 module.exports = { deleteTeacherSchedule, updateTeacherSchedule, insertTeacherSchedule, findTeachersSchedule, findTeacherScheduleToSpecificTeacher }
-
--- routers/teacherSchedule.js
+++ routers/teacherSchedule.js
@@ -2,45 +2,59 @@ const express = require('express')
 const router = express.Router()
 const { insertTeacherSchedule, updateTeacherSchedule, findTeachersSchedule, findTeacherScheduleToSpecificTeacher } = require('../modules/teacher_schedule')
 
-
-
 // insert
 router.post('/insertTeacherSchedule', express.json(), async (req, res) => {
-    let ans = await insertTeacherSchedule(req.body)
-    if (ans)
-        res.status(200).send(ans)
+    try {
+        const ans = await insertTeacherSchedule(req.body)
+        if (ans)
+            res.status(200).send(ans.data)
+        else
+            res.status(500).send({ message: 'no insert' })
+    }
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
 
-
-// // insert
-
+// update
 router.post('/updateTeacherSchedule', express.json(), async (req, res) => {
-    console.log(req.body)
-    let ans = await updateTeacherSchedule(req.body.name, req.body.update)
-    console.log(ans);
-    res.send(ans)
+    try {
+        let ans = await updateTeacherSchedule(req.body.name, req.body.update)
+        if (ans)
+            res.status(200).send(ans.data)
+        else
+            res.status(500).send({ message: 'no update' })
+    }
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
-
-
-
 
 // read 
 router.get('/findTeachersSchedule', async (req, res) => {
-    const ans = await findTeachersSchedule(req.query)
-    if (ans)
-        res.status(200).send(ans)
-    else
-        res.status(404).send("not found")
+    try {
+        const ans = await findTeachersSchedule(req.query)
+        if (ans)
+            res.status(200).send(ans)
+        else
+            res.status(500).send({ message: "not found" })
+    }
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
-
 
 router.get('/findTeacherScheduleToSpecificTeacher/:name', async (req, res) => {
-    const ans = await findTeacherScheduleToSpecificTeacher(req.params.name)
-    if (ans)
-        res.status(200).send(ans)
-    else
-        res.status(404).send("not found")
+    try {
+        const ans = await findTeacherScheduleToSpecificTeacher(req.params.name)
+        if (ans)
+            res.status(200).send(ans)
+        else
+            res.status(500).send({ message: "not found" })
+    }
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
-
 
 module.exports = router
