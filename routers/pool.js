4fefc6e88d21f9b53cc58469eae574baed9fcb33
stsparctice <131611269+stsparctice@users.noreply.github.com>
2023-08-28 01:57:10 +0300

    changes

--- routers/pool.js
+++ routers/pool.js
@@ -27,6 +27,7 @@ router.get('/find/:poolName', async (req, res) => {
 router.get('/getAll',  async (req, res) => {
     try {
         let ans = await find({ disabled: 0})
+        console.log({ans})
         res.status(200).send(ans)
     }
     catch (error) {
