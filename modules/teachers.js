f2b62873c0f773455a9ebde6c4e6c3195c26bc9b
rl130260 <rutlev260@gmail.com>
2023-10-25 11:53:25 +0300

    validation

--- modules/teachers.js
+++ modules/teachers.js
@@ -4,9 +4,43 @@ require('dotenv').config()
 const { deleteTeacherSchedule } = require('./teacher_schedule')
 const { dbServer, getData, postData } = require('../services/axios');
 
+const teacherType = {
+    ID: { name: 'id', type: 'number', required: true },
+    TeacherName: { name: 'teacherName', type: 'nvarchar', required: true },
+    Phone: { name: 'phone', type: 'nvarchar', required: false },
+    Email: { name: 'email', type: 'nvarchar', required: false },
+    Address: { name: 'address', type: 'nvarchar', required: false },
+    City: { name: 'city', type: 'nvarchar', required: false },
+    Gender: { name: 'gender', type: 'number', required: false },
+    Annotation: { name: 'annotation ', type: 'nvarchar', required: false },
+    AddedDate: { name: 'addedDate', type: 'datetime ', required: true },
+    UserName: { name: 'userName', type: 'nvarchar', required: true },
+    Disabled: { name: 'disabled', type: 'number', required: false },
+    DisabledUser: { name: 'disabledUser', type: 'nvarchar', required: false },
+    DisableReason: { name: 'disableReason', type: 'nvarchar', required: false }
+}
+
 // insert  //
 async function insertTeacher(obj) {
+    let flag=false;
+    let missingValues=[]
     try {
+        for (let key in teacherType) {
+            if(teacherType[key].required==true){
+                for(let key2 in obj){
+                    if(teacherType[key].name.toLocaleLowerCase()==key2.toLocaleLowerCase()){
+                        flag=true;
+                    }
+                }
+                if(flag==false){
+                    missingValues.push(teacherType[key].name)
+                }
+                flag=false
+            }
+        }
+        if(missingValues.length>0){
+            throw new Error(`The missing values are:${missingValues}`)
+    }
         const name = await postData(dbServer, '/read/readMany/Teachers', { condition: { TeacherName: `'${obj.TeacherName}'`, Disabled: 0 } })
         if (name.data[0]) {
             throw new Error("the name is exist")
